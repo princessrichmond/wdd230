0c6dab8a4e840551d9e7103c1afffc308a37a222
princessrichmond <princessdianemanalili@gmnail.com>
2024-06-23 19:00:27 -0400

    update week02

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,15 @@ document.addEventListener('DOMContentLoaded', function(){
     var lastModifiedElement = document.getElementById("lastModified");
     lastModifiedElement.textContent += " " + lastModifiedDate;
 }); 
+
+document.getElementById('hamburger').addEventListener('click', function () {
+    document.getElementById('nav').classList.toggle('active');
+});
+
+document.getElementById('darkmode-toggle').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+    document.querySelector('header').classList.toggle('dark-mode');
+    document.querySelector('nav').classList.toggle('dark-mode');
+    document.querySelector('main').classList.toggle('dark-mode');
+    document.querySelector('footer').classList.toggle('dark-mode');
+});
