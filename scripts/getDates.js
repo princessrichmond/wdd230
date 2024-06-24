fc988e27b1481a7889a17c7f333453bea74816fa
princessrichmond <princessdianemanalili@gmnail.com>
2024-06-23 21:09:42 -0400

    update button

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,9 +4,6 @@ document.addEventListener('DOMContentLoaded', function(){
     lastModifiedElement.textContent += " " + lastModifiedDate;
 }); 
 
-document.getElementById('hamburger').addEventListener('click', function () {
-    document.getElementById('nav').classList.toggle('active');
-});
 
 document.getElementById('darkmode-toggle').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
@@ -14,4 +11,17 @@ document.getElementById('darkmode-toggle').addEventListener('click', function ()
     document.querySelector('nav').classList.toggle('dark-mode');
     document.querySelector('main').classList.toggle('dark-mode');
     document.querySelector('footer').classList.toggle('dark-mode');
+});
+
+
+document.getElementById('hamburger').addEventListener('click', function () {
+    document.getElementById('nav').classList.toggle('active');
+    document.getElementById('close').style.display = 'block';
+    document.getElementById('hamburger').style.display = 'none';
+});
+
+document.getElementById('close').addEventListener('click', function () {
+    document.getElementById('nav').classList.toggle('active');
+    document.getElementById('close').style.display = 'none';
+    document.getElementById('hamburger').style.display = 'block';
 });
