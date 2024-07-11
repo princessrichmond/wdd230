8c68fa6550d35f9f2f90ff65de8eef2054868009
princessrichmond <princessdianemanalili@gmnail.com>
2024-07-11 18:37:11 -0400

    update

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,39 +5,39 @@ document.addEventListener('DOMContentLoaded', function(){
 }); 
 
 
-document.getElementById('darkmode-toggle').addEventListener('click', function () {
-    document.body.classList.toggle('dark-mode');
-    document.querySelector('header').classList.toggle('dark-mode');
-    document.querySelector('nav').classList.toggle('dark-mode');
-    document.querySelector('main').classList.toggle('dark-mode');
-    document.querySelector('footer').classList.toggle('dark-mode');
-});
+    document.getElementById('darkmode-toggle').addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
+        document.querySelector('header').classList.toggle('dark-mode');
+        document.querySelector('nav').classList.toggle('dark-mode');
+        document.querySelector('main').classList.toggle('dark-mode');
+        document.querySelector('footer').classList.toggle('dark-mode');
+    });
 
 
-document.getElementById('hamburger').addEventListener('click', function () {
-    document.getElementById('nav').classList.toggle('active');
-    document.getElementById('close').style.display = 'block';
-    document.getElementById('hamburger').style.display = 'none';
-});
+    document.getElementById('hamburger').addEventListener('click', function () {
+        document.getElementById('nav').classList.toggle('active');
+        document.getElementById('close').style.display = 'block';
+        document.getElementById('hamburger').style.display = 'none';
+    });
 
-document.getElementById('close').addEventListener('click', function () {
-    document.getElementById('nav').classList.toggle('active');
-    document.getElementById('close').style.display = 'none';
-    document.getElementById('hamburger').style.display = 'block';
-});
+    document.getElementById('close').addEventListener('click', function () {
+        document.getElementById('nav').classList.toggle('active');
+        document.getElementById('close').style.display = 'none';
+        document.getElementById('hamburger').style.display = 'block';
+    });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const temperature = 75;
-    document.getElementById('temperature').textContent = temperature;
-});
+    // document.addEventListener('DOMContentLoaded', function() {
+    //     const temperature = 75;
+    //     document.getElementById('temperature').textContent = temperature;
+    // });
 
-const visitsDisplay = document.querySelector(".visits");
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
-}
-numVisits++;
-localStorage.setItem("numVisits-ls", numVisits);
+    const visitsDisplay = document.querySelector(".visits");
+    let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+    if (numVisits !== 0) {
+        visitsDisplay.textContent = numVisits;
+    } else {
+        visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+    }
+    numVisits++;
+    localStorage.setItem("numVisits-ls", numVisits);
 
--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,11 @@
-const baseURL = "https://github.com/princessrichmond/wdd230/";
-const linksURL = "https://github.com/princessrichmond/wdd230/data/links.json";
+const baseURL = "https://princessrichmond.github.io/wdd230/";
+const linksURL = "https://princessrichmond.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     console.log(data);
-    displayLinks(data);
+    displayLinks(data.weeks);
   }
 
 function displayLinks(weeks) {
@@ -14,21 +14,21 @@ const learningActivities = document.querySelector('.card ul');
 weeks.forEach(week => {
     const weekItem = document.createElement('li');
     const weekHeader = document.createElement('strong');
-    weekHeader.textContent = week.week;
+    weekHeader.textContent = `${week.week}: `;
     weekItem.appendChild(weekHeader);
 
     week.links.forEach((link, index) => {
     const linkElement = document.createElement('a');
     linkElement.href = resolveURL(link.url);
+    linkElement.setAttribute("class", "link");
     linkElement.textContent = link.title;
-
+    weekItem.appendChild(linkElement);
     if (index < week.links.length - 1) {
         const separator = document.createElement('span');
         separator.textContent = ' | ';
         weekItem.appendChild(separator);
     }
 
-    weekItem.appendChild(linkElement);
     });
 
     learningActivities.appendChild(weekItem);
